cb34134013ed0d1c34f64627abc42057ce85bc15
Sunny Rai <sunnyrai31@gmail.com>
2025-07-29 21:21:38 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -6,14 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      
           Learn React
-        </a>
+    
       </header>
     </div>
   );
